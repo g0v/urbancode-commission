d0a572e4d8f5003ba74eb7b9059283f9698bc81e
Richard Hsieh <monaludao@gmail.com>
2015-07-05 17:22:59 +0800

    download tainan comission record

--- tnup.R
+++ tnup.R
@@ -1,4 +1,4 @@
-tpup_parse1<-function(){
+tnup_parse1<-function(){
     library(bitops)
     library(RCurl)
     library(XML)
@@ -9,13 +9,18 @@ tpup_parse1<-function(){
     
     item <- xpathSApply(get_url_parse,"//table[@class='p1']/tr/td/a",xmlValue)
     url <- xpathSApply(get_url_parse,"//table[@class='p1']/tr/td/a[@href]",xmlAttrs)
-    link <- data.frame(item,url[1,],stringsAsFactors = FALSE)
+    link <- data.frame(url[1,],item,stringsAsFactors = FALSE)
+    link[,2]<-iconv(link[,2],"utf-8","big5")
+    
+    filetype <- c()
     
     for(i in 1:nrow(link)){
-        filetype <- c(filetype, checkfiletype(weblist[i,1]))  
+        filetype <- c(filetype, checkfiletype(link[i,1]))
     }
     
-    write.csv(link,"tn_link_list.csv",row.names=FALSE)
+    link <- cbind(link,filetype)
+    
+    write.csv(link,"tnup_link_list.csv",row.names=FALSE)
 }
 
 checkfiletype <- function(txt){
@@ -24,4 +29,25 @@ checkfiletype <- function(txt){
     } else if(grepl(".*doc", txt)==TRUE) {
         return("doc")
     }
+}
+
+tn_dlrecord<-function(csvfile){
+    library(httr)
+    
+    link.list<-read.csv(csvfile,stringsAsFactors=FALSE)
+    
+    if (!dir.exists("./record/tnup/raw/")) {
+        dir.create("./record/tnup/")
+        dir.create("./record/tnup/raw/")
+    }
+    
+    pb <- txtProgressBar(max = nrow(link.list), style = 3)
+    
+    for(i in 1:nrow(link.list)){
+        link<-GET(link.list[i,1])
+        filelocale<-paste("./record/tnup/raw/",link.list[i,2],".",link.list[i,3],sep="")
+        download.file(link$url,filelocale,mode="wb")
+        
+        setTxtProgressBar(pb, i)
+    }    
 }
