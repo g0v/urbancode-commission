edefaeaeb450367c54b57cef87dffa8af6cf738b
Richard Hsieh <monaludao@gmail.com>
2015-07-08 18:05:13 +0800

    new-taipei up try out

---
+++ npup.R
@@ -0,0 +1,6 @@
+##xpup
+
+##FILE NAME
+##xpathSApply(get_url_parse,"//form[@name='fdownload']/table/tr/td/table/tbody/tr/td/div",xmlValue)
+##URL
+##xpathSApply(get_url_parse,"//form[@name='fdownload']/table/tr/td/table/tbody/tr/td/div/a",xmlAttrs)
