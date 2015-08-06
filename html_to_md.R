65725156f6129b2c26f2179860602f3131d3b879
Richard Hsieh <monaludao@gmail.com>
2015-08-06 18:09:24 +0800

    progress html data parser

--- html_to_md.R
+++ html_to_md.R
@@ -38,22 +38,25 @@ html_parse<-function(){
         item.list[[i]]<-parse.done[item.ind[i]:(item.ind[i+1]-1),]
     }
     
-    item.list[[3]]<-item_parse(item.list[[3]])
+    item.list<-lapply(item.list,item_parse)
     
     return(item.list)
 }
 
 item_parse<-function(item.df){
-    case.ind<-c(grep("^審議事項",item.df[,1]),nrow(item.df)+1)
+    case.ind<-c(grep("^報告事項|^審議事項",item.df[,1]),nrow(item.df)+1)
     case.cnt<-length(case.ind)-1
     
     case.list<-rep(list(NULL),case.cnt)
     
-    for(i in 1:case.cnt){
-        case.list[[i]]<-item.df[case.ind[i]:(case.ind[i+1]-1),]
-    }
-    
-    case.list<-lapply(case.list,case_parse)
+    if(case.cnt==0){
+        case.list[[1]]<-paragraph_parse(item.df)
+    } else {
+        for(i in 1:case.cnt){
+            case.list[[i]]<-item.df[case.ind[i]:(case.ind[i+1]-1),]
+        }
+        case.list<-lapply(case.list,case_parse)
+    }    
     
     return(case.list)
 }
@@ -61,15 +64,20 @@ item_parse<-function(item.df){
 case_parse<-function(case.df){
     table.ind<-grep("意見綜理表$",case.df[,1])
     
-    content.list<-list(case.df[1:(table.ind-1),],case.df[table.ind:nrow(case.df),])
+    if(length(table.ind)!=0){
+        content.list<-list(case.df[1:(table.ind-1),],case.df[table.ind:nrow(case.df),])
+    } else {
+        content.list<-list(case.df)
+    }
     
-    section.ind<-c(1,grep("說明：$|^決議：|決議：$",content.list[[1]][,1]),nrow(content.list[[1]])+1)
+    section.ind<-c(1,grep("說明：$|^決議|決議：$",content.list[[1]][,1]),nrow(content.list[[1]])+1)
     section.cnt<-length(section.ind)-1
     
     section.list<-rep(list(NULL),section.cnt)
+    names(section.list)<-content.list[[1]][section.ind[1:(length(section.ind))-1],1]
     
     for(i in 1:section.cnt){
-        section.list[[i]]<-content.list[[1]][section.ind[i]:(section.ind[i+1]-1),]
+        section.list[[i]]<-content.list[[1]][(section.ind[i]+1):(section.ind[i+1]-1),]
     }
     
     content.list[[1]]<-section.list
@@ -80,13 +88,17 @@ case_parse<-function(case.df){
 }
 
 paragraph_parse<-function(section.df){
-    para.ind<-c(1,grep(paste("^(十)?",zh_number,"、",collapse="|",sep=""),section.df[,1]),nrow(section.df)+1)
+    para.ind<-c(grep(paste("^(十)?",zh_number,"、",collapse="|",sep=""),section.df[,1]),nrow(section.df)+1)
     para.cnt<-length(para.ind)-1
-    
+        
     para.list<-rep(list(NULL),para.cnt)
    
-    for(i in 1:para.cnt){
-        para.list[[i]]<-section.df[para.ind[i]:(para.ind[i+1]-1),]
+    if(para.cnt==0){
+        para.list[[1]]<-paste(section.df[,1],collapse="",sep="")
+    } else {
+        for(i in 1:para.cnt){
+            para.list[[i]]<-paste(section.df[para.ind[i]:(para.ind[i+1]-1),1],collapse="",sep="")
+        }
     }
     
     return(para.list)
