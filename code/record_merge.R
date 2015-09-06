a992d61722035813dbdb59c8668b1ec83d94a6e2
Richard Hsieh <monaludao@gmail.com>
2015-09-06 16:03:59 +0800

    update json convertor

--- code/record_merge.R
+++ code/record_merge.R
@@ -2,12 +2,17 @@ record_merge<-function(region,start,end){
     n.vector<-c(start:end)
     f.vector<-vector(mode="character",length=length(n.vector))
     
+    for(i in start:end){
+        print(i)
+        if(i!=657){json_convert(record_parse(i),i)}
+    }
+    
     for(i in 1:length(n.vector)){
-        f.name<-paste(as.character(n.vector[i]),".json",sep="")
+        f.name<-paste("./record/TPEUP/JSON/",as.character(n.vector[i]),".json",sep="")
         f.vector[i]<-readLines(f.name)
     }
     
     jsontxt<-paste("[",paste(f.vector,collapse=",",sep=""),"]",sep="")
     
-    write(jsontxt,file="./record/TPEUP/TPEUP.json")
+    write(jsontxt,file="./record/TPEUP/TPEUP_first_parse.json")
 }
