b98af40109523cd5f387bd43813f7252cd84dbe3
Richard Hsieh <monaludao@gmail.com>
2015-09-11 15:52:48 +0800

    update parser

--- code/record_merge.R
+++ code/record_merge.R
@@ -1,15 +1,22 @@
-record_merge<-function(region,start,end){
+mass_parse<-function(region,start,end){
     n.vector<-c(start:end)
     f.vector<-vector(mode="character",length=length(n.vector))
     
     for(i in start:end){
         print(i)
-        if(i!=657){json_convert(record_parse(i),i)}
+        json_convert(record_parse(i),i)
     }
+}
+
+record_merge<-function(region,start,end){
+    n.vector<-c(start:end)
+    f.vector<-vector(mode="character",length=length(n.vector))
+    
     
     for(i in 1:length(n.vector)){
         f.name<-paste("./record/TPEUP/JSON/",as.character(n.vector[i]),".json",sep="")
-        f.vector[i]<-readLines(f.name)
+        f.vector[i]<-paste(readLines(f.name),collapse="",sep="")
+        
     }
     
     jsontxt<-paste("[",paste(f.vector,collapse=",",sep=""),"]",sep="")
