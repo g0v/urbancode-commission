e84a227e1e07d8ea7a34f397be749cf1a4b82394
Richard Hsieh <monaludao@gmail.com>
2016-03-28 15:27:17 +0800

    renew TPQUP list

--- code/TPQ_UP.R
+++ code/TPQ_UP.R
@@ -6,31 +6,28 @@ npup_parse1<-function(){
     
     link_list<-c()
     
+    pb <- txtProgressBar(max = 16, style = 3)
+    
     for (i in 1:16) {
         url<-paste0("http://www.planning.ntpc.gov.tw/download/?type_id=10479&parent_id=10160&page=",i)
-        #url<-paste("http://www.planning.ntpc.gov.tw/ap/fdownload/fdownload2.jsp?mfkind_id=00038&enpage=",i,sep="")
         get_url<-getURL(url, encoding="utf-8")
         get_url_parse<-htmlParse(get_url, encoding="utf-8")
         
-        #value_parse<-xpathSApply(get_url_parse,"//form[@name='fdownload']/table/tr/td/table/tbody/tr/td[@class='font-2']",xmlValue)
         value_parse<-xpathSApply(get_url_parse,"//table[@id='download_box']//tr//td",xmlValue)
         file_name<-as.character(iconv(value_parse[seq(6,length(value_parse),6)],"utf-8","big5"))
         
-        #for(j in 1:length(file_name)){
-            #file_name[j]<-gsub(" \t","",file_name[j])
-        #}
-        
         link<-xpathSApply(get_url_parse,"//table[@id='download_box']//tr//td//a[@href]",xmlAttrs)[3,]
-        #link<-xpathSApply(get_url_parse,"//form[@name='fdownload']/table/tr/td/table/tbody/tr/td/div/a[@href]",xmlAttrs)[1,]
         link<-paste0("www.planning.ntpc.gov.tw/download/",link)
         
         file_type<-substr(link,nchar(link)-2,nchar(link))
-        #file_type<-value_parse[seq(4,length(value_parse),8)]
         
         list<-data.frame(link=link,name=file_name,type=file_type,stringsAsFactors=FALSE)
         
         link_list<-rbind(link_list,list)
+        
+        setTxtProgressBar(pb, i)
     }
+    link_list<-unique(link_list)
     write.csv(link_list,"./record/TPQUP/npup_link_list.csv",row.names=FALSE)
 }
 
